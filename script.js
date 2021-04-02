60794f651dd409e414a9c21d355f7e87ab180e9c
Tann-R <vagabondsentinel@gmail.com>
2021-04-02 12:03:40 -0500

    First image update.

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function active(nav) {
 
 let home = '<h1>Exploration - Discovery - Learning</h1>' +
     '<h2>"I don\'t want to believe.  I want to know." -Carl Sagan</h2>' +
-    '<img src="https://img.nickpic.host/ajJyp1.png" alt="A portrait of a bespectacled miqo\'te with white hair and blue eyes" title="Ilyanith Vhas, created with picrew.me" />' +
+    '<img src="./resources/ilya-at-limsa-1.jpg" alt="A closeup of a miqo\'te woman outside of Limsa Lominsa" title="Ilyanith Vhas" />' +
     '<div class="textbox">' +
     '<p>This will be content.</p>' +
     '</div>'
