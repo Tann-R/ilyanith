59200290706d68de68242ee3d6690cfd3f9e79c7
Tann-R <vagabondsentinel@gmail.com>
2021-04-02 12:36:16 -0500

    Minor styling update, updated content in home and demographics.

--- script.js
+++ script.js
@@ -23,18 +23,28 @@ let home = '<h1>Exploration - Discovery - Learning</h1>' +
     '<h2>"I don\'t want to believe.  I want to know." -Carl Sagan</h2>' +
     '<img src="./resources/ilya-at-limsa-1.jpg" alt="A closeup of a miqo\'te woman outside of Limsa Lominsa" title="Ilyanith Vhas" />' +
     '<div class="textbox">' +
-    '<p>This will be content.</p>' +
+    '<p>With an insatiable curiosity and an agile mind, Ilya is a clever and resourceful woman, perpetually eager to explore and discover ever more about the world around her.' +
+    '  She is eternally questioning, both within and without, and more than one haples victim has been trapped beneath a flurry of questions.  Despite her willingness to pursue ' +
+    'information, however, she should not be mistaken for outgoing; indeed, unless her curiosity is well piqued, it is rare for her to approach a stranger.  Instead, in crowds, ' +
+    'she can most often be found occupying her time with people-watching silently.</p>' +
+    '<p>Though not shy, as such, Ilyanith is socially awkward and frequently socially inept (and oblivious).  As such, she is prone to missing sometimes seemingly obvious social' +
+    ' cues, apologizes often to excess out of an uncertainty as to whether or not she has commited some social faux pas, and blushes frequently and easily, much to her chagrin.</p>' +
+    '<p>None of this, however, should be taken to mean that she is a fearful creature.  Though she seeks to avoid such awkwardness where possible, given the proper motivation, ' +
+    'she can be fiercely passionate and even bold.  Ilya is marked by a strong, possibly overdeveloped sense of justice, quick to stick up for the underdog and only too willing to' +
+    ' leap to the defense of the perceived defenseless, whether they ask it of her or no - a tendency that has more than once gotten her into trouble.</p>'
     '</div>'
 
 let demographics = '<h1>Exploration - Discovery - Learning</h1>' +
     '<h2>"I don\'t want to believe.  I want to know." -Carl Sagan</h2>' +
     '<img src="./resources/ilya-in-fc-house-1.jpg" alt="A closeup of a miqo\'te woman sitting in an industrial structure" title="Ilyanith Vhas" />' +
     '<div class="textbox">' +
-    '<p><b>Nickname(s)/AKA:</b> Ilya, Yani</p>' +
-    '<p><b>Age:</b> tbd</p>' +
-    '<p><b>Birthplace:</b> Ishgard</p>' +
-    '<p><b>Class:</b> machinist, astrologian</p>' +
-    '<p><b>Role:</b> damage, healer</p>' +
+    '<table>' +
+    '<tr><td><b>Nickname(s) / AKA</b></td><td><b>: </b>Ilya, Yani</td></tr>'
+    '<tr><td><b>Age</b></td><td><b>: </b>30</td></tr>' +
+    '<tr><td><b>Birthplace</b></td><td><b>: </b>Ishgard</td></tr>' +
+    '<tr><td><b>Job</b></td><td><b>: </b> machinist, astrologian</td></tr>' +
+    '<tr><td><b>Role</b></td><td><b>: </b>damage, healer</td></tr>' +
+    '</table>' +
     '</div>'
 
 let history = '<h1>Exploration - Discovery - Learning</h1>' +
