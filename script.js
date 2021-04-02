7f45ba2a91dbb7e86827706f4a9e481767375eac
Tann-R <vagabondsentinel@gmail.com>
2021-04-02 11:30:32 -0500

    Initial commit.

---
+++ script.js
@@ -0,0 +1,51 @@
+let activePg="home"
+function active(nav) {
+  if (nav!=activePg) {
+    document.getElementById(activePg).className="nav-inactive";
+    document.getElementById(nav).className="nav-active";
+    activePg=nav;
+    if (nav=="home") {
+      document.getElementById("main").innerHTML=home;
+    }
+    if (nav=="demographics") {
+      document.getElementById("main").innerHTML=demographics;
+    }
+    if (nav=="history") {
+      document.getElementById("main").innerHTML=history;
+    }
+    if (nav=="gallery") {
+      document.getElementById("main").innerHTML=gallery;
+    }
+  }
+}
+
+let home = '<h1>Exploration - Discovery - Learning</h1>' +
+    '<h2>"I don\'t want to believe.  I want to know." -Carl Sagan</h2>' +
+    '<img src="https://img.nickpic.host/ajJyp1.png" alt="A portrait of a bespectacled miqo\'te with white hair and blue eyes" title="Ilyanith Vhas, created with picrew.me" />' +
+    '<div class="textbox">' +
+    '<p>This will be content.</p>' +
+    '</div>'
+
+let demographics = '<h1>Exploration - Discovery - Learning</h1>' +
+    '<h2>"I don\'t want to believe.  I want to know." -Carl Sagan</h2>' +
+    '<img src="https://img.nickpic.host/ajJyp1.png" alt="A portrait of a bespectacled miqo\'te with white hair and blue eyes" title="Ilyanith Vhas, created with picrew.me" />' +
+    '<div class="textbox">' +
+    '<p><b>Nickname(s)/AKA:</b> Ilya, Yani</p>' +
+    '<p><b>Age:</b> tbd</p>' +
+    '<p><b>Birthplace:</b> Ishgard</p>' +
+    '<p><b>Class:</b> machinist, astrologian</p>' +
+    '<p><b>Role:</b> damage, healer</p>' +
+    '</div>'
+
+let history = '<h1>Exploration - Discovery - Learning</h1>' +
+    '<h2>"I don\'t want to believe.  I want to know." -Carl Sagan</h2>' +
+    '<div class="journalbox">' +
+    '<p>Forthcoming.</p>' +
+    '</div>'
+
+let gallery = '<h1>Exploration - Discovery - Learning</h1>' +
+    '<h2>"I don\'t want to believe.  I want to know." -Carl Sagan</h2>' +
+    '<img src="https://img.nickpic.host/ajJyp1.png" alt="A portrait of a bespectacled miqo\'te with white hair and blue eyes" title="Ilyanith Vhas, created with picrew.me" />' +
+    '<div class="textbox">' +
+    '<p>Forthcoming.</p>' +
+    '</div>'
